d953c385bcd11f112b32772cfc84e171efab592b
Nerii10 <96701056+Nerii10@users.noreply.github.com>
2025-01-06 00:11:38 +0100

    e

--- src/assets/WeatherImages.jsx
+++ src/assets/WeatherImages.jsx
@@ -17,29 +17,30 @@ export function WeatherImages({ WeatherInfo, Hour, error}) {
       >
         {/* Wybór odpowiedniego obrazka na podstawie warunków */}
         <img
-          src={
-            Desc.includes("clear")
-              ? Hour >= 18
-                ? "/WeatherApp/Moon.png"
-                : "/WeatherApp/Sun.png"
-              : Desc.includes("clouds") || Desc.includes("cloudy") || Desc.includes("smoke")
-              ? Hour >= 18
-                ? "/WeatherApp/CloudNight.png"
-                : "/WeatherApp/CloudDay.png"
-              : Desc.includes("snow")
-              ? Hour >= 18
-                ? "/WeatherApp/SnowflakeNight.png"
-                : "/WeatherApp/SnowflakeDay.png"
-              : Desc.includes("haze") || Desc.includes("mist")
-              ? "/WeatherApp/Mist.png"
-              : Desc.includes("rain")
-              ? Hour >= 18
-                ? "/WeatherApp/RainNight.png"
-                : "/WeatherApp/RainDay.png"
-              : ""
-          }
-          className="WeatherIcon"
-        />
+  src={
+    Desc.includes("clear")
+      ? Hour >= 18 || Hour < 3
+        ? "/WeatherApp/Moon.png"
+        : "/WeatherApp/Sun.png"
+      : Desc.includes("clouds") || Desc.includes("cloudy") || Desc.includes("smoke")
+      ? Hour >= 18 || Hour < 3
+        ? "/WeatherApp/CloudNight.png"
+        : "/WeatherApp/CloudDay.png"
+      : Desc.includes("snow")
+      ? Hour >= 18 || Hour < 3
+        ? "/WeatherApp/SnowflakeNight.png"
+        : "/WeatherApp/SnowflakeDay.png"
+      : Desc.includes("haze") || Desc.includes("mist")
+      ? "/WeatherApp/Mist.png"
+      : Desc.includes("rain")
+      ? Hour >= 18 || Hour < 3
+        ? "/WeatherApp/RainNight.png"
+        : "/WeatherApp/RainDay.png"
+      : ""
+  }
+  className="WeatherIcon"
+/>
+
       </motion.div>
     ) : (
       <motion.div
